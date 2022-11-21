860b836c5e9ad2f9291aa7111919dedf57e9fd69
IreneZhou0129 <xiaoxinzhou0@gmail.com>
2022-11-20 20:16:09 -0500

    added an exception handler

--- get_libs.py
+++ get_libs.py
@@ -13,18 +13,28 @@ def main(filename):
     finder.run_script(filename)
     # find uninstalled modules
     uninstalled_libs = [l for l in finder.badmodules.keys()]
-    print(f'Packages {uninstalled_libs} are missing.')
+
+    print("\n\n")
+    print(f'Missing packages:\n {uninstalled_libs}')
+
+    failed_libs = []
 
     # install each lib
     for i in uninstalled_libs:
-        # implement pip as a subprocess:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', i])
-        
-        # process output with an API in the subprocess module:
-        reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-        installed_libs = [r.decode().split('==')[0] for r in reqs.split()]
-        
-        print(f'Successfully installed: {installed_libs}.')
+        try:
+            # implement pip as a subprocess:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', i])
+            
+            # process output with an API in the subprocess module:
+            reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+            installed_libs = [r.decode().split('==')[0] for r in reqs.split()]
+            
+            print(f'Successfully installed: {installed_libs}')
+        except Exception as e:
+            failed_libs.append({i:e})
+
+    print(f"Failed installing:\n {failed_libs}")
+            
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Filename")
